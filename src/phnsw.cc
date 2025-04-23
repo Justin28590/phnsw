447e2046b3f73f99ba1f660d29b8815fa02d5e74
Zeng GuangYi <tgy_scut2021@outlook.com>
2025-04-23 19:55:24 +0800

    Each time reduce W or C size for 1.

--- src/phnsw.cc
+++ src/phnsw.cc
@@ -2,7 +2,7 @@
  * @Author: Zeng GuangYi tgy_scut2021@outlook.com
  * @Date: 2024-11-10 00:22:53
  * @LastEditors: Zeng GuangYi tgy_scut2021@outlook.com
- * @LastEditTime: 2025-03-24 14:22:03
+ * @LastEditTime: 2025-04-18 16:01:45
  * @FilePath: /phnsw/src/phnsw.cc
  * @Description: phnsw Core Component
  * 
@@ -527,7 +527,8 @@ int Phnsw::inst_rmc(void *rd_temp_ptr, void *rd2_temp_ptr, uint32_t *stage_now) 
     }
     (*rd_ptr)[loops - 1] = 0;
     (*rd2_ptr)[loops - 1] = 0;
-    // TODO reduce C_size by 1
+    uint32_t *X_size = (uint32_t *) Phnsw::Registers.find_match("C_size", rd_size);
+    *X_size = *X_size - 1;
     return 0;
 }
 
@@ -572,7 +573,8 @@ int Phnsw::inst_rmw(void *rd_temp_ptr, void *rd2_temp_ptr, uint32_t *stage_now) 
     }
     (*rd_ptr)[loops - 1] = 0;
     (*rd2_ptr)[loops - 1] = 0;
-    // TODO reduce W_size by 1
+    uint32_t *X_size = (uint32_t *) Phnsw::Registers.find_match("W_size", rd_size);
+    *X_size = *X_size - 1;
     return 0;
 }
 
